4a3be20a0f8870ac5eedc2299a100af6753f4b3d
sae-github <miyasae1126@gmail.com>
2021-07-20 22:36:25 +0900

    fix:index.html style.css index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,38 @@
-const globalNav = document.querySelectorAll('a[href^="#"]');
-for(let i = 0; i < globalNav.length; i++) {
-  const globalNavItem = globalNav[i];
-  globalNavItem.addEventListener('click', function(e) {
-    e.preventDefault();
-    const headerHeight = document.getElementById('js-header').clientHeight; 
-    const href = globalNavItem.getAttribute('href');
-    if(href == "#" || href == "") {
-      'html';
-      window.scrollTo( {
-        top: 0,
-        behavior: "smooth"
-      });
-    } else {
-      const sectionName = href.replace('#',"");
-      const sectionId = document.getElementById(sectionName);
-      const rect = sectionId.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      const target = rect + offset - headerHeight;
-      window.scrollTo( {
-        top: target,
-        behavior: "smooth"
-      });
-    }
+const globalNav = document.getElementById('js-global-nav-list');
+globalNav.addEventListener('click', function (e) {
+  e.preventDefault();
+  const globalNavItem = e.target;
+  const headerHeight = document.getElementById('js-header').clientHeight;
+  const href = globalNavItem.getAttribute('href');
+  const sectionName = href.replace('#', "");
+  const sectionId = document.getElementById(sectionName);
+  const rect = sectionId.getBoundingClientRect().top;
+  const offset = window.pageYOffset;
+  const target = rect + offset - headerHeight;
+  window.scrollTo({
+    top: target,
+    behavior: "smooth"
   });
-}
-window.addEventListener('scroll', function() {
-  const topUp = document.getElementById('topup');
-  const scroll = window.pageYOffset;
-  if (scroll > 100) {
-    topUp.classList.add('is-show');
-  } else {
-    topUp.classList.remove('is-show');
-  }
 });
+
+const topUp = document.getElementById('topup');
+topUp.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+})
+
+const callback = (entries) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      topUp.classList.add('is-show');
+    } else {
+      topUp.classList.remove('is-show');
+    }
+  })
+}
+const observer = new IntersectionObserver(callback);
+observer.observe(document.getElementById('section3'));
+
